a6e5b5fc486fcdb1de498963e79cfb57b7f9fc09
David Toro <davidesteban0736@.com>
2024-11-30 15:30:22 -0500

    se acaba script de Despres

--- DESPRES.R
+++ DESPRES.R
@@ -54,22 +54,11 @@ plot(Modelosimple)#heterrosedástico porque se observa un embudo
 #n<50 shapiro
 #n>=50 kolmo
 
-#Shapiro n<50
-
-#el error es la diferencia entre el valor observaado y el obtenido ES EL RESIDUAL
-Error=residuals(Modelosimple)
-hist(Error)#vamos a analizar el histograma del comportamiento de los residuales
-#esperamos que se comporten dem manera normal, ver si se asemeja a campana de GAUSS o NO
-shapiro.test(Error)#W= VALOR CALCULADO p-valor=0,05239   alpha=0,05   p-valor>alfa (NO RECHAZO Ho) los residuos sí se distribuyen normalente
-#El test es más confiable
-
 #kol n>=50
 
 install.packages("nortest")
 library("nortest")
 lillie.test(Error)#en este caso no aplica para nuestra base de datos por el número de datos. HACEMOS LA COMPARACIÓN CON EL P-VALOR DE NUEVO
-
-
 
 #Validación de homosedasticidad
 install.packages("lmtest")
@@ -79,8 +68,11 @@ bptest(Modelosimple,studentize = FALSE)#P-valor<alfa rechazho Ho, las varianzas 
 
 
 
+#Hacemos la estadística descriptiva
 
 summary(X1)
+var(X1)
+sd(X1)
 
 # Crear el boxplot
 boxplot(X1, 
@@ -93,57 +85,3 @@ boxplot(X1,
 abline(v = mean(X1), col = "red", lty = 2)
 
 
-
-
-
-
-
-Modelomultiple=lm(Y2~X1+X2)#corresponde a los coeficientes del modelo de la línea recta
-
-#nos arroja el intercepto 
-#nos arroja un X
-
-plot(X,Y,col="red")#diagrama de dispersión
-abline(Modelomultiple)#arroja la línea
-
-cor(X,Y)#coef Correlación con el signo negativo o positivo. Nega-> si son inversamente proporcipnales
-#fuerte,medio,débil->en este caso es fuerte
-
-summary(Modelomultiple)#nos saca los residuales y no los interpretamos
-#nos arroja los coeficientes
-#r cuadrado
-#p-valor...para la prueba de hipótesis de linealidad
-
-plot(Modelomultiple)#heterrosedástico porque se observa un embudo
-#Q-Q
-#RESIDUALS VS FITTED 
-
-
-#VAMOS A hacer validación de supuestos
-#validación de normalidad
-#la puedo validad con Q-Q o con dos pruebas, kolmo y shapiro (obtenemos un p valor)
-#me arroja un p valor, si es menor que alfa rechazo Ho, sino no rechazo Ho
-#n<50 shapiro
-#n>=50 kolmo
-
-#Shapiro n<50
-
-#el error es la diferencia entre el valor observaado y el obtenido ES EL RESIDUAL
-Errorm=residuals(Modelomultiple)
-hist(Errorm)#vamos a analizar el histograma del comportamiento de los residuales
-#esperamos que se comporten dem manera normal, ver si se asemeja a campana de GAUSS o NO
-shapiro.test(Errorm)#W= VALOR CALCULADO p-valor=0,05239   alpha=0,05   p-valor>alfa (NO RECHAZO Ho) los residuos sí se distribuyen normalente
-#El test es más confiable
-
-#kol n>=50
-
-install.packages("nortest")
-library("nortest")
-lillie.test(Error)#en este caso no aplica para nuestra base de datos por el número de datos. HACEMOS LA COMPARACIÓN CON EL P-VALOR DE NUEVO
-
-
-#Validación de homosedasticidad
-install.packages("lmtest")
-library("lmtest")
-bptest(Modelomultiple,studentize = FALSE)#P-valor<alfa rechazho Ho, las varianzas son heterosedásticas...
-#validamos con los gráficos y analizamos los indicadores como lo hemos hecho en otras clases
